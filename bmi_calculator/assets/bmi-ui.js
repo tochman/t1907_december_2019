93b1e3be5a0a799c8e611859e110a00d04e48d6e
Thomas <thomas@craftacademy.se>
2019-12-05 16:44:42 +0100

    adds styling adds keypress event adds content

--- bmi_calculator/assets/bmi-ui.js
+++ bmi_calculator/assets/bmi-ui.js
@@ -2,8 +2,15 @@ const calculateBMI = () => {
   let resultsElement = document.getElementById('results')
   let weightInput = document.getElementById('weight')
   let heightInput = document.getElementById('height')
-  // debugger
   let results = calculateMetricBMI(weightInput.value, heightInput.value)
   let message = getBMIMessage(results)
-  resultsElement.innerHTML = `<h1>Your BMI is ${results} and you are deemed ${message}</h1>`
+  resultsElement.innerHTML = `<h2>Your BMI is ${results} and you are deemed ${message}</h2>`
+}
+
+const handlePress = () => {
+  if (event.keyCode === 13) {
+    calculateBMI()
+  } else {
+    return
+  }
 }
